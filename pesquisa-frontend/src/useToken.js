53689f2d6b857c4951a9d0b04d09b1154d87b4b9
Roberto <rg.almeida2012@gmail.com>
2022-05-16 11:25:33 -0300

    Salvando o token de autenticacao nos cookies do navegador

--- pesquisa-frontend/src/useToken.js
+++ pesquisa-frontend/src/useToken.js
@@ -1,15 +1,37 @@
 import { useState } from 'react';
 
 export default function useToken() {
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
   const getToken = () => {
-    const tokenString = localStorage.getItem('token');
+    const tokenString = getCookie('token');
     return tokenString
   };
 
   const [token, setToken] = useState(getToken());
 
   const saveToken = userToken => {
-    localStorage.setItem('token', JSON.stringify(userToken));
+    setCookie("token", userToken, 1)
     setToken(userToken.token);
   };
 
